36581df132bc6ac4fa393265158e12509e137185
Habibur Rahman <habib.swi16@gmail.com>
2023-08-30 16:21:25 +0600

    category navbar

---
+++ app.js
@@ -0,0 +1,21 @@
+const handleCategory = async () => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/news/categories`
+  );
+  const data = await response.json();
+
+  const tabContainer = document.getElementById("tab-container");
+
+  data.data.news_category.slice(0, 3).forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <a class="tab  text-black">${category.category_name}</a>
+    
+    `;
+
+    tabContainer.appendChild(div);
+  });
+  console.log(data.data.news_category);
+};
+
+handleCategory();
